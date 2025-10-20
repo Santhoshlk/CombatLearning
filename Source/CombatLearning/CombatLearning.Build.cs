371e246a13787d75eef5d624fd868f46bf87cc17
Santhoshlk <santhoshlukka123@gmail.com>
2025-10-20 05:38:52 +0530

    I have Started Doing Combat

---
+++ Source/CombatLearning/CombatLearning.Build.cs
@@ -0,0 +1,51 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class CombatLearning : ModuleRules
+{
+	public CombatLearning(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG",
+			"Slate"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"CombatLearning",
+			"CombatLearning/Variant_Platforming",
+			"CombatLearning/Variant_Platforming/Animation",
+			"CombatLearning/Variant_Combat",
+			"CombatLearning/Variant_Combat/AI",
+			"CombatLearning/Variant_Combat/Animation",
+			"CombatLearning/Variant_Combat/Gameplay",
+			"CombatLearning/Variant_Combat/Interfaces",
+			"CombatLearning/Variant_Combat/UI",
+			"CombatLearning/Variant_SideScrolling",
+			"CombatLearning/Variant_SideScrolling/AI",
+			"CombatLearning/Variant_SideScrolling/Gameplay",
+			"CombatLearning/Variant_SideScrolling/Interfaces",
+			"CombatLearning/Variant_SideScrolling/UI"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ Source/CombatLearning/CombatLearning.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CombatLearning.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CombatLearning, "CombatLearning" );
+
+DEFINE_LOG_CATEGORY(LogCombatLearning)
