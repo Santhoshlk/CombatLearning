03bde10294d81e37633e932417c88b8c0f1d0a9c
Santhoshlk <santhoshlukka123@gmail.com>
2025-10-21 16:18:29 +0530

    I added The GameModeBase And started coding of the character

---
+++ Source/CombatLearning/Public/GameMode/CombatClassGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CombatClassGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMBATLEARNING_API ACombatClassGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
