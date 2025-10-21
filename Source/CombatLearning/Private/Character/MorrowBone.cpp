8b5da084eb76acca3a3e03052e92717b7bcd1c64
Santhoshlk <santhoshlukka123@gmail.com>
2025-10-21 18:40:20 +0530

    Camera Component and  the Required Work Before Binding Of the Character Has Been Done

--- Source/CombatLearning/Private/Character/MorrowBone.cpp
+++ Source/CombatLearning/Private/Character/MorrowBone.cpp
@@ -3,9 +3,49 @@
 
 #include "Character/MorrowBone.h"
 #include "CombatDebugHelper.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+AMorrowBone::AMorrowBone()
+{
+	//get Set the size and Half Height
+	Capsule=GetCapsuleComponent();
+	SetRootComponent(Capsule);
+
+	//attach the skeleton Mesh to Capsule
+	SkeletonMesh=GetMesh();
+	SkeletonMesh->SetupAttachment(Capsule);
+
+	//before creating the spring arm first set The Rotation of the Character none
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	// now set the Spring Arm to Rotate and of course create it
+	SpringArm=CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->bUsePawnControlRotation  = true;
+	// now create the follow camera and do have it no rotation
+	FollowCamera=CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->bUsePawnControlRotation = false;
+
+	//setup Attachments of both Camera and spring Arm
+	SpringArm->SetupAttachment(Capsule);
+	FollowCamera->SetupAttachment(SpringArm,USpringArmComponent::SocketName);
+
+	//now have the character only orient to move
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	//some settings to make the things better
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f);
+	GetCharacterMovement()->MaxWalkSpeed = 500.0f;
+	GetCharacterMovement()->BrakingDecelerationWalking=2000.f;
+	
+	
+}
 
 void AMorrowBone::BeginPlay()
 {
 	Super::BeginPlay();
-	 Debug::PrintMessage(FString(TEXT("The Debug Helper is Working")));
+	Debug::PrintMessage(FString(TEXT("The Debug Helper is Working")));
 }
--- Source/CombatLearning/Public/Character/MorrowBone.h
+++ Source/CombatLearning/Public/Character/MorrowBone.h
@@ -6,9 +6,14 @@
 #include "Character/MorrowBoneClassBase.h"
 #include "MorrowBone.generated.h"
 
+class UCameraComponent;
+class USpringArmComponent;
+class UCapsuleComponent;
+class USkeletalMeshComponent;
 /**
  * 
  */
+
 UCLASS()
 class COMBATLEARNING_API AMorrowBone : public AMorrowBoneClassBase
 {
@@ -16,4 +21,24 @@ class COMBATLEARNING_API AMorrowBone : public AMorrowBoneClassBase
 public:
 	// u can still have Begin play even if u delete it from the base class it is a right of every unreal class
 	virtual void BeginPlay() override;
+	AMorrowBone();
+
+	
+private:
+	//to divide into regions new trick by Vince Petrel
+#pragma region component
+
+	//now add all ur components in that region
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,meta=(AllowPrivateAccess="true"),Category="CameraBoom")
+	TObjectPtr<USpringArmComponent> SpringArm;
+
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,meta=(AllowPrivateAccess="true"),Category="FollowCamera")
+	TObjectPtr<UCameraComponent> FollowCamera;
+
+	UPROPERTY()
+	TObjectPtr<UCapsuleComponent> Capsule;
+
+	UPROPERTY()
+	TObjectPtr<USkeletalMeshComponent> SkeletonMesh;
+#pragma endregion 
 };
