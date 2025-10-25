0d20d6bf3e3d859f300adcd2d539c953db727e3d
Santhoshlk <santhoshlukka123@gmail.com>
2025-10-25 20:47:54 +0530

    I added All the Class Hierarchy Of the AnimInstance

---
+++ Source/CombatLearning/Public/Character/Hero/MorrowBoneHeroAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/MorrowBoneCharacterAnimInstance.h"
+#include "MorrowBoneHeroAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMBATLEARNING_API UMorrowBoneHeroAnimInstance : public UMorrowBoneCharacterAnimInstance
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/CombatLearning/Public/Character/MorrowBoneLinkedAnimLayer.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/MorrowBoneBaseAnimInstance.h"
+#include "MorrowBoneLinkedAnimLayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMBATLEARNING_API UMorrowBoneLinkedAnimLayer : public UMorrowBoneBaseAnimInstance
+{
+	GENERATED_BODY()
+	
+};
