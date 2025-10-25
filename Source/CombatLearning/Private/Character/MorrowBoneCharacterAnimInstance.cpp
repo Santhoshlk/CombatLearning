645a27cc6c378d44dd9f2b2944f74760ed43b4cd
Santhoshlk <santhoshlukka123@gmail.com>
2025-10-25 21:24:37 +0530

    The Logic For The Anim Instance class is done

--- Source/CombatLearning/Private/Character/MorrowBoneCharacterAnimInstance.cpp
+++ Source/CombatLearning/Private/Character/MorrowBoneCharacterAnimInstance.cpp
@@ -2,4 +2,25 @@
 
 
 #include "Character/MorrowBoneCharacterAnimInstance.h"
+#include "Character/MorrowBoneClassBase.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
+void UMorrowBoneCharacterAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	OwningPtr=Cast<AMorrowBoneClassBase>(TryGetPawnOwner());
+	checkf(OwningPtr,TEXT("You were Not able To Successfully cast to the owning ptr"));
+	OwningCharacterMovement=OwningPtr->GetCharacterMovement();
+}
+
+void UMorrowBoneCharacterAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+	//check if Owning Ptr and Owning Character Movement is valid
+	if (IsValid(OwningPtr) && OwningCharacterMovement  )
+	{
+		GroundSpeed=OwningCharacterMovement->Velocity.Size2D();
+		bHasAcceleration=OwningCharacterMovement->GetCurrentAcceleration().SizeSquared2D()>0.f;
+	}
+}
--- Source/CombatLearning/Public/Character/MorrowBoneCharacterAnimInstance.h
+++ Source/CombatLearning/Public/Character/MorrowBoneCharacterAnimInstance.h
@@ -6,6 +6,8 @@
 #include "Character/MorrowBoneBaseAnimInstance.h"
 #include "MorrowBoneCharacterAnimInstance.generated.h"
 
+class UCharacterMovementComponent;
+class AMorrowBoneClassBase;
 /**
  * 
  */
@@ -13,5 +15,27 @@ UCLASS()
 class COMBATLEARNING_API UMorrowBoneCharacterAnimInstance : public UMorrowBoneBaseAnimInstance
 {
 	GENERATED_BODY()
-	
+
+public:
+	//If u have doubt That UNeed To call Super Then Go and Check The Signature
+	virtual void NativeInitializeAnimation() override;
+
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+#pragma region Components
+	UPROPERTY()
+	TObjectPtr<AMorrowBoneClassBase> OwningPtr;
+
+	UPROPERTY()
+	TObjectPtr<UCharacterMovementComponent> OwningCharacterMovement;
+#pragma endregion
+
+#pragma region Variables
+
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category="Speed")
+	float GroundSpeed;
+
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category="Acceleration")
+	bool bHasAcceleration;
+#pragma endregion
 };
