9e2525f1a54c2e9ba819bb0d7e780bf5ea72021c
Santhoshlk <santhoshlukka123@gmail.com>
2025-10-29 09:47:38 +0530

    The AnimInstance and ABP of the Base Character has been Complete

--- Source/CombatLearning/Private/Character/Hero/MorrowBoneHeroAnimInstance.cpp
+++ Source/CombatLearning/Private/Character/Hero/MorrowBoneHeroAnimInstance.cpp
@@ -3,3 +3,31 @@
 
 #include "Character/Hero/MorrowBoneHeroAnimInstance.h"
 
+#include "Character/MorrowBone.h"
+
+void UMorrowBoneHeroAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	//we need a current reference to our actual ptr
+	if (OwningPtr)
+	{
+		OwningPlayerCharacter=Cast<AMorrowBone>(OwningPtr);
+		
+	}
+	
+}
+
+void UMorrowBoneHeroAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaTime)
+{
+	Super::NativeThreadSafeUpdateAnimation(DeltaTime);
+	if (bHasAcceleration)
+	{
+		IdleElapsedTime=0.f;
+		bShouldEnterRelaxedState=false;
+	}
+	else
+	{
+		IdleElapsedTime+=DeltaTime;
+		bShouldEnterRelaxedState=(IdleElapsedTime>=EnterRelaxedStateThreshold);
+	}
+}
--- Source/CombatLearning/Public/Character/Hero/MorrowBoneHeroAnimInstance.h
+++ Source/CombatLearning/Public/Character/Hero/MorrowBoneHeroAnimInstance.h
@@ -6,6 +6,7 @@
 #include "Character/MorrowBoneCharacterAnimInstance.h"
 #include "MorrowBoneHeroAnimInstance.generated.h"
 
+class AMorrowBone;
 /**
  * 
  */
@@ -13,5 +14,32 @@ UCLASS()
 class COMBATLEARNING_API UMorrowBoneHeroAnimInstance : public UMorrowBoneCharacterAnimInstance
 {
 	GENERATED_BODY()
+
+public:
+#pragma region Functions
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaTime) override;
+#pragma endregion
+
+	//to add the state of MorrowBone to its AnimInstance
+	//U need a reference to the Character itself
+#pragma region Components
+     UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category="Character")
+	  AMorrowBone* OwningPlayerCharacter;
+
+#pragma endregion 
+
+
+#pragma region Variables
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category="Anim data | Relax")
+	bool bShouldEnterRelaxedState;
+
+	//u should have some float one to see the threshold and one to calculate elapsed
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category="Anim Data | Relax")
+	float EnterRelaxedStateThreshold=5.f;
+
+	
+	float IdleElapsedTime;
+#pragma endregion
 	
 };
