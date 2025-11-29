79e5b9d96a010f994bfa0164739117f0d5ae22ed
Santhoshlk <santhoshlukka123@gmail.com>
2025-11-29 18:49:29 +0530

    I added GamePlay Ability Class and GameplayAbility Activation Policies

---
+++ Source/CombatLearning/Private/GameplayAbility/MorrowBoneGameplayAbility.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameplayAbility/MorrowBoneGameplayAbility.h"
+#include "AbilitySystem/MorrowBoneAbilitySystemComponent.h"
+
+
+void UMorrowBoneGameplayAbility::OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilitySpec& Spec)
+{
+	Super::OnGiveAbility(ActorInfo, Spec);
+	//FGameplayAbilityActorInfo is a great struct which can have a lot of Info about our actor
+	//i we want we acn include our ASC header and  get Some Functions
+	if (ActivationPolicy==EMorrowBoneAbilityActivationPolicy::onGiven && !Spec.IsActive())
+	{
+		//to Activate Ability
+		ActorInfo->AbilitySystemComponent->TryActivateAbility(Spec.Handle);
+	}
+	
+}
+
+void UMorrowBoneGameplayAbility::EndAbility(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+	bool bReplicateEndAbility, bool bWasCancelled)
+{
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+	if (ActivationPolicy==EMorrowBoneAbilityActivationPolicy::onGiven)
+	{
+		if (ActorInfo)
+		{
+			ActorInfo->AbilitySystemComponent->ClearAbility(Handle);
+		}
+	}
+}
+
---
+++ Source/CombatLearning/Public/GameplayAbility/MorrowBoneGameplayAbility.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "MorrowBoneGameplayAbility.generated.h"
+UENUM(BlueprintType)
+enum class EMorrowBoneAbilityActivationPolicy : uint8
+{
+	onTriggered,
+	onGiven
+};
+/**
+ * 
+ */
+UCLASS()
+class COMBATLEARNING_API UMorrowBoneGameplayAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditDefaultsOnly, Category = "ActivationPolicy")
+	EMorrowBoneAbilityActivationPolicy ActivationPolicy = EMorrowBoneAbilityActivationPolicy::onTriggered;
+
+	virtual void OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec);
+
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled);
+	
+};
