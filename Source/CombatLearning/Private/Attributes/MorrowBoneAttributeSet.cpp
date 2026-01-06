bc3432202bf389ebda07b3ff506fe6d1c61030b6
Santhoshlk <santhoshlukka123@gmail.com>
2026-01-06 22:53:23 +0530

    I learned about Attribute Set and i setup the class and its attributes.

--- Source/CombatLearning/Private/Attributes/MorrowBoneAttributeSet.cpp
+++ Source/CombatLearning/Private/Attributes/MorrowBoneAttributeSet.cpp
@@ -3,3 +3,14 @@
 
 #include "Attributes/MorrowBoneAttributeSet.h"
 
+UMorrowBoneAttributeSet::UMorrowBoneAttributeSet()
+{
+	InitCurrentHealth(1.0F);
+	InitMaxHealth(10.0F);
+	InitCurrentRage(1.0f);
+	InitMaxRage(10.0f);
+	InitCurrentStamina(1.0f);
+	InitMaxStamina(10.0f);
+	InitAttackPower(15.0f);
+	InitDefensePower(15.0f);
+}
--- Source/CombatLearning/Public/Attributes/MorrowBoneAttributeSet.h
+++ Source/CombatLearning/Public/Attributes/MorrowBoneAttributeSet.h
@@ -4,8 +4,15 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystem/MorrowBoneAbilitySystemComponent.h"
 #include "MorrowBoneAttributeSet.generated.h"
-
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+  
+ 	
 /**
  * 
  */
@@ -13,5 +20,43 @@ UCLASS()
 class COMBATLEARNING_API UMorrowBoneAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
-	
+
+public:
+
+	UMorrowBoneAttributeSet();
+
+protected:
+
+	//now write attribute accessor to make these functions
+	UPROPERTY(BlueprintReadOnly,Category="Health")
+	FGameplayAttributeData CurrentHealth;
+	ATTRIBUTE_ACCESSORS(UMorrowBoneAttributeSet,CurrentHealth)
+
+	UPROPERTY(BlueprintReadOnly,Category="Health")
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UMorrowBoneAttributeSet,MaxHealth)
+
+	UPROPERTY(BlueprintReadOnly,Category="Rage")
+	FGameplayAttributeData CurrentRage;
+	ATTRIBUTE_ACCESSORS(UMorrowBoneAttributeSet,CurrentRage)
+
+	UPROPERTY(BlueprintReadOnly,Category="Rage")
+	FGameplayAttributeData MaxRage;
+	ATTRIBUTE_ACCESSORS(UMorrowBoneAttributeSet,MaxRage)
+
+	UPROPERTY(BlueprintReadOnly,Category="AP")
+	FGameplayAttributeData AttackPower;
+	ATTRIBUTE_ACCESSORS(UMorrowBoneAttributeSet,AttackPower)
+
+	UPROPERTY(BlueprintReadOnly,Category="DP")
+	FGameplayAttributeData DefensePower;
+	ATTRIBUTE_ACCESSORS(UMorrowBoneAttributeSet,DefensePower)
+
+	UPROPERTY(BlueprintReadOnly,Category="Stamina")
+	FGameplayAttributeData  CurrentStamina;
+	ATTRIBUTE_ACCESSORS(UMorrowBoneAttributeSet,CurrentStamina)
+
+	UPROPERTY(BlueprintReadOnly,Category="Stamina")
+	FGameplayAttributeData  MaxStamina;
+	ATTRIBUTE_ACCESSORS(UMorrowBoneAttributeSet,MaxStamina)
 };
