9b1ef18f3c7186a1be8d8061c58293a0e7e52f53
Santhoshlk <santhoshlukka123@gmail.com>
2026-01-16 23:27:48 +0530

    I added the same attributes to relevant Attributes to capture using a better and more same method using macros

--- Source/CombatLearning/Private/GEEx_Calculation/GEEx_Calculation_DamageTaken.cpp
+++ Source/CombatLearning/Private/GEEx_Calculation/GEEx_Calculation_DamageTaken.cpp
@@ -4,18 +4,32 @@
 #include "GEEx_Calculation/GEEx_Calculation_DamageTaken.h"
 #include "Attributes/MorrowBoneAttributeSet.h"
 
+//create a struct for handling the declaration.
+
+struct FMorrowBoneDamageDataCapture
+{
+	//first of all declare  them
+	DECLARE_ATTRIBUTE_CAPTUREDEF(AttackPower)
+	DECLARE_ATTRIBUTE_CAPTUREDEF(DefensePower)
+
+	FMorrowBoneDamageDataCapture()
+	{
+		// now define them
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UMorrowBoneAttributeSet,AttackPower,Source,false)
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UMorrowBoneAttributeSet,DefensePower,Source,false)
+	}
+};
+
+// create a static func and variable definition as the values need to be captured only once
+static const FMorrowBoneDamageDataCapture& GetMorrowBoneDamageDataCapture()
+{
+	static FMorrowBoneDamageDataCapture Data;
+	return Data;
+}
 UGEEx_Calculation_DamageTaken::UGEEx_Calculation_DamageTaken()
 {
-	FProperty* AttackPowerProperty=FindFieldChecked<FProperty>(
-		UMorrowBoneAttributeSet::StaticClass(),
-		GET_MEMBER_NAME_CHECKED(UMorrowBoneAttributeSet,AttackPower));
-
-	// now Create a GameplayEffectAttributeCaptureDefinition For it
-	FGameplayEffectAttributeCaptureDefinition AttackPowerCaptureDefinition(
-		AttackPowerProperty,
-		EGameplayEffectAttributeCaptureSource::Source,
-		false);
-
-	// then add this to relevant attributes to capture
-	RelevantAttributesToCapture.Add(AttackPowerCaptureDefinition);
+	// add this to relevant attributes to capture
+	RelevantAttributesToCapture.Add(GetMorrowBoneDamageDataCapture().AttackPowerDef);
+	RelevantAttributesToCapture.Add(GetMorrowBoneDamageDataCapture().DefensePowerDef);
+	
 }
