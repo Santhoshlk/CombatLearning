1fa05e7dd6b2916274e5b21342864e885d480ec7
Santhoshlk <santhoshlukka123@gmail.com>
2026-02-05 01:01:43 +0530

    I added Progress Bar Widget in the Process of making UI.

--- Source/CombatLearning/Private/Widgets/MorrowBoneWidgetBase.cpp
+++ Source/CombatLearning/Private/Widgets/MorrowBoneWidgetBase.cpp
@@ -10,7 +10,6 @@
 void UMorrowBoneWidgetBase::NativeOnInitialized()
 {
 	Super::NativeOnInitialized();
-
 	// now when the Widget gets initialized then we can get the holding of UI component and the values it Broadcasts
 	if (IPawnUIInterface* PawnUIInterface =  Cast<IPawnUIInterface>(GetOwningPlayerPawn()))
 	{
