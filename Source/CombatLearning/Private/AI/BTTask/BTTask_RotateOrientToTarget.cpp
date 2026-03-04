75be348dc7e4daa89d4de7ae010139ad8b0f1705
Santhoshlk <santhoshlukka123@gmail.com>
2026-03-04 19:04:01 +0530

    I created a BTTask Rotate Orient To Target so that enemy can rotate freely and smoothly with the help of Motion Warping Towards MorrowBone.

--- Source/CombatLearning/Private/AI/BTTask/BTTask_RotateOrientToTarget.cpp
+++ Source/CombatLearning/Private/AI/BTTask/BTTask_RotateOrientToTarget.cpp
@@ -3,7 +3,11 @@
 
 #include "AI/BTTask/BTTask_RotateOrientToTarget.h"
 
+#include "AIController.h"
+
+#include "BehaviorTree/BlackboardComponent.h"
 #include "BehaviorTree/BlackboardData.h"
+#include "Kismet/KismetMathLibrary.h"
 
 UBTTask_RotateOrientToTarget::UBTTask_RotateOrientToTarget()
 {
@@ -34,9 +38,80 @@ void UBTTask_RotateOrientToTarget::InitializeFromAsset(UBehaviorTree& Asset)
 	
 }
 
+uint16 UBTTask_RotateOrientToTarget::GetInstanceMemorySize() const
+{
+	// we need to give this so that the memory would allocate teh proper amount at the time of executing task
+	return sizeof(FOnMemoryRotateOrientToTarget);
+}
+
+EBTNodeResult::Type UBTTask_RotateOrientToTarget::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	UObject* TargetObject = OwnerComp.GetBlackboardComponent()->GetValueAsObject(InTargetKey.SelectedKeyName);
+	AActor* TargetActor = Cast<AActor>(TargetObject);
+	APawn* OwningPawn = OwnerComp.GetAIOwner()->GetPawn();
+	checkf(TargetActor && OwningPawn , TEXT("the Target Actor or the owning pawn is not valid "));
+	FOnMemoryRotateOrientToTarget* Memory = CastInstanceNodeMemory<FOnMemoryRotateOrientToTarget>(NodeMemory);
+	check(Memory);
+	Memory->OwningPawn = OwningPawn;
+	Memory->TargetActor = TargetActor;
+
+	// even now the pointers are not valid the task has been failed
+	if (!Memory->isValid())
+	{
+		return EBTNodeResult::Failed;
+	}
+    if (IsInAnglePrecision(Memory->OwningPawn.Get(),Memory->TargetActor.Get()))
+    {
+	    // now task has concluded call reset and finish the task
+    	Memory->Reset();
+    	return EBTNodeResult::Succeeded;
+    }
+   // now to do the actual work in tick 
+	return EBTNodeResult::InProgress;
+	
+}
+
+void UBTTask_RotateOrientToTarget::TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	// nothing in the base class
+	// tick task comest after execute task so the memory value is obviously set
+	// to finish task in tick use FinishLatentTask
+	FOnMemoryRotateOrientToTarget* Memory = CastInstanceNodeMemory<FOnMemoryRotateOrientToTarget>(NodeMemory);
+	check(Memory);
+	if (!Memory->isValid())
+	{
+		FinishLatentTask(OwnerComp,EBTNodeResult::Failed);
+	}
+	else if (IsInAnglePrecision(Memory->OwningPawn.Get(),Memory->TargetActor.Get()))
+	{
+		// Task has succeeded
+		Memory->Reset();
+		FinishLatentTask(OwnerComp,EBTNodeResult::Succeeded);
+	}
+	else
+	{
+		// now do the actual rotation
+		//we change the rotation from owning pawn to target actor
+		const FRotator FinalRotation = UKismetMathLibrary::FindLookAtRotation(Memory->OwningPawn->GetActorLocation(),Memory->TargetActor->GetActorLocation());
+		FRotator InterpRotation = FMath::RInterpTo(Memory->OwningPawn->GetActorRotation(),FinalRotation,DeltaSeconds,RotationInterpSpeed);
+		Memory->OwningPawn->SetActorRotation(InterpRotation);
+		// whole interpolation process
+	}
+	
+}
+
 FString UBTTask_RotateOrientToTarget::GetStaticDescription() const
 {
 	const FString KeyName = InTargetKey.SelectedKeyName.ToString();
 	return FString::Printf(TEXT("Rotate Orient to Face Target KeyName :  %s , Angle Precision : %s"),*KeyName,*FString::SanitizeFloat(AnglePrecision));
 	 
+}
+
+bool UBTTask_RotateOrientToTarget::IsInAnglePrecision( const APawn* QueryPawn,const  AActor* TargetActor) const
+{
+	const FVector QueryToTarget = (TargetActor->GetActorLocation() - QueryPawn->GetActorLocation()).GetSafeNormal();
+	float AngleToCheck = FVector::DotProduct(QueryToTarget,QueryPawn->GetActorForwardVector());
+    // this will be in -1 to 1 so now change to degrees by taking cos inverse
+     float Angle = UKismetMathLibrary::DegAcos(AngleToCheck);
+	return Angle <= AnglePrecision;
 }
--- Source/CombatLearning/Public/AI/BTTask/BTTask_RotateOrientToTarget.h
+++ Source/CombatLearning/Public/AI/BTTask/BTTask_RotateOrientToTarget.h
@@ -9,6 +9,25 @@
 /**
  * 
  */
+struct FOnMemoryRotateOrientToTarget
+{
+    //	Structs default access is public 
+	// a place to store pointers that just required until not null
+	TWeakObjectPtr<APawn> OwningPawn;
+	TWeakObjectPtr<AActor> TargetActor;
+	bool isValid()
+	{
+		return OwningPawn.IsValid() && TargetActor.IsValid();
+	}
+   void Reset()
+	{
+		OwningPawn = nullptr;
+		TargetActor = nullptr;
+	}
+	
+};
+
+
 UCLASS()
 class COMBATLEARNING_API UBTTask_RotateOrientToTarget : public UBTTaskNode
 {
@@ -18,7 +37,17 @@ class COMBATLEARNING_API UBTTask_RotateOrientToTarget : public UBTTaskNode
 
 	virtual void InitializeFromAsset(UBehaviorTree& Asset) override;
 
+	/** size of instance memory */
+	virtual uint16 GetInstanceMemorySize() const override;
+
+	//**execute task**//
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+	virtual void TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+	
 	virtual FString GetStaticDescription() const override;
+ 
+     bool IsInAnglePrecision(const APawn* QueryPawn, const AActor* TargetActor) const;	
 	
 	// u need some values
 	UPROPERTY(EditAnywhere,Category = "Rotate Orient To Target")
