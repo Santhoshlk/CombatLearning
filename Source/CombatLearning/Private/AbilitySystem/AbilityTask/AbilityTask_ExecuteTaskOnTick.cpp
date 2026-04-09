813c9c6cdc3dee3beee7d2b6aff46f4334af5f89
Santhoshlk <santhoshlukka123@gmail.com>
2026-04-09 16:46:17 +0530

    I learned how to create an Ability Task and used it to do ticking

---
+++ Source/CombatLearning/Private/AbilitySystem/AbilityTask/AbilityTask_ExecuteTaskOnTick.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/AbilityTask/AbilityTask_ExecuteTaskOnTick.h"
+
+
+UAbilityTask_ExecuteTaskOnTick::UAbilityTask_ExecuteTaskOnTick()
+{
+	bTickingTask = true;
+}
+
+UAbilityTask_ExecuteTaskOnTick* UAbilityTask_ExecuteTaskOnTick::ExecuteTaskOnTick(
+	UGameplayAbility* OwningGameplayAbility)
+{
+	// Create the New Ability Task
+	UAbilityTask_ExecuteTaskOnTick* TickTask = NewAbilityTask<UAbilityTask_ExecuteTaskOnTick>(OwningGameplayAbility);
+	return TickTask;
+}
+
+void UAbilityTask_ExecuteTaskOnTick::TickTask(float DeltaTime)
+{
+	Super::TickTask(DeltaTime);
+	// The output is the Broadcasted value
+   TickTaskDelegate.Broadcast(DeltaTime);
+	
+}
+
+
---
+++ Source/CombatLearning/Public/AbilitySystem/AbilityTask/AbilityTask_ExecuteTaskOnTick.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/Tasks/AbilityTask.h"
+#include "AbilityTask_ExecuteTaskOnTick.generated.h"
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnTickTaskDelegate,float, time);
+
+
+/**
+ * 
+ */
+UCLASS()
+class COMBATLEARNING_API UAbilityTask_ExecuteTaskOnTick : public UAbilityTask
+{
+	GENERATED_BODY()
+
+public:
+
+  // Create the delegate variable
+  UPROPERTY(BlueprintAssignable)
+	FOnTickTaskDelegate TickTaskDelegate;
+	
+	UAbilityTask_ExecuteTaskOnTick();
+
+	// Generate the Static function which Calls the Ability Task
+	UFUNCTION(BlueprintCallable,Category = "Ability Task ",meta=(HidePin = "OwningGameplayAbility",DisplayName = "ExecuteTaskOnTick",DefaultToSelf = "OwningGameplayAbility",BlueprintInternalUseOnly = "true"))
+	static UAbilityTask_ExecuteTaskOnTick* ExecuteTaskOnTick(UGameplayAbility* OwningGameplayAbility);
+
+	/** Tick function for this task, if bTickingTask == true */
+	virtual void TickTask(float DeltaTime) override;	
+};
