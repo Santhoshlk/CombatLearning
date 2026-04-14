241d0b1ea5a411de34a9dcfc7e67d9aa592c1f15
Santhoshlk <santhoshlukka123@gmail.com>
2026-04-14 23:51:39 +0530

    Both Overlap and Hit Works.

--- Source/CombatLearning/Private/Projecile/MorrowBoneProjectileBase.cpp
+++ Source/CombatLearning/Private/Projecile/MorrowBoneProjectileBase.cpp
@@ -64,6 +64,12 @@ void AMorrowBoneProjectileBase::OnProjectileHit(UPrimitiveComponent* HitComponen
 void AMorrowBoneProjectileBase::OnProjectileOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
 	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+	if (OtherActor)
+	{
+		Debug::PrintMessage(OtherActor->GetActorNameOrLabel());
+	}
+	// after the Interaction with Projectile
+	Destroy();
 }
 
 
