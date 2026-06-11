0301b1d7d7d06639d95afeee482cc6f8f0cf930c
Santhoshlk <santhoshlukka123@gmail.com>
2026-06-11 23:56:03 +0530

    I created the NativeCooldown Latent Action Class.

---
+++ Source/CombatLearning/Public/LatentAction/MorrowBoneCooldownLatentAction.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MorrowBoneEnums.h"
+
+class FNativeCooldownLatentAction : public FPendingLatentAction
+{
+  bool m_toCancel;
+	float m_TotalCooldownTime;
+	float m_UpdateInterval;
+	float& m_RemainingTime;
+	ECooldownActionsOutput& m_OutputCooldownActions;
+	// these are latent input action connections
+	FName ExecutionFunction;
+	int32 OutputLink;
+	FWeakObjectPtr CallBackTarget;
+	float ElapsedInterval;
+	float ElapsedTimeSinceStart;
+
+	
+public:
+	FNativeCooldownLatentAction(float TotalCooldownTime,float UpdateInterval,float& RemainingTime,ECooldownActionsOutput& OutputCooldownActions,FLatentActionInfo LatentActionInfo) :m_toCancel(false),m_TotalCooldownTime(TotalCooldownTime),
+	m_UpdateInterval(UpdateInterval),m_RemainingTime(RemainingTime),m_OutputCooldownActions(OutputCooldownActions),
+	ExecutionFunction(LatentActionInfo.ExecutionFunction),
+	OutputLink(LatentActionInfo.Linkage),
+	CallBackTarget(LatentActionInfo.CallbackTarget),
+	ElapsedInterval(0.f),
+	ElapsedTimeSinceStart(0.f)
+	{
+		
+	}
+
+};
