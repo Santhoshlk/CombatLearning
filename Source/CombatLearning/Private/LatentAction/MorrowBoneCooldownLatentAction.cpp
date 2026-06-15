414f37c339c8d4a4e43f20f17a315bf843446cc7
Santhoshlk <santhoshlukka123@gmail.com>
2026-06-15 23:12:50 +0530

    I added the cooldown logic so to the latent action.

--- Source/CombatLearning/Private/LatentAction/MorrowBoneCooldownLatentAction.cpp
+++ Source/CombatLearning/Private/LatentAction/MorrowBoneCooldownLatentAction.cpp
@@ -2,6 +2,7 @@
 
 
 #include "LatentAction/MorrowBoneCooldownLatentAction.h"
+#include "CombatDebugHelper.h"
 
 
 void FNativeCooldownLatentAction::Cancel()
@@ -11,7 +12,44 @@ void FNativeCooldownLatentAction::Cancel()
 
 void FNativeCooldownLatentAction::UpdateOperation(FLatentResponse& Response)
 {
-  
 	
-	FPendingLatentAction::UpdateOperation(Response);
+	if (m_toCancel)
+	{
+		m_OutputCooldownActions = ECooldownActionsOutput::Canceled;
+
+		Response.FinishAndTriggerIf(true,ExecutionFunction,OutputLink,CallBackTarget);
+		return;
+	}
+	if (ElapsedTimeSinceStart >= m_TotalCooldownTime)
+	{
+		m_OutputCooldownActions = ECooldownActionsOutput::Completed;
+
+		// this is something we call when we finish the latent action
+		Response.FinishAndTriggerIf(true,ExecutionFunction,OutputLink,CallBackTarget);
+		return;
+	}
+	if (ElapsedInterval <= m_UpdateInterval)
+	{
+		// Elapsed time is like delta time in tick
+		ElapsedInterval+=Response.ElapsedTime();
+		
+	}
+	else
+	{
+		if (m_UpdateInterval==0.f)
+		{
+			ElapsedTimeSinceStart+=Response.ElapsedTime();
+			
+		}
+		else
+		{
+			ElapsedTimeSinceStart+=m_UpdateInterval;
+		}
+		m_RemainingTime = m_TotalCooldownTime - ElapsedTimeSinceStart;
+		
+		m_OutputCooldownActions = ECooldownActionsOutput::Updated;
+		// u just want to call trigger
+		Response.TriggerLink(ExecutionFunction,OutputLink,CallBackTarget);
+	}
+	
 }
